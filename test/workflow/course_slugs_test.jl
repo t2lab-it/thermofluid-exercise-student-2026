b1fda2b2f1c0cf21d40480323a199e4b01074ee6
ryo-ARAKI <araki.ryo@rs.tus.ac.jp>
2026-08-13 12:01:18 +0200

    feat: combine F03 and F04 submission workflow

--- test/workflow/course_slugs_test.jl
+++ test/workflow/course_slugs_test.jl
@@ -6,8 +6,7 @@ include(joinpath(SLUG_REPO_ROOT, "scripts", "course.jl"))
 @testset "curriculum branch slugs" begin
     @test SLUGS == Dict(
         "F02" => "julia-arrays-and-tests",
-        "F03" => "vector-calculus",
-        "F04" => "numerical-differentiation",
+        "F03-F04" => "vector-calculus-numerical-differentiation",
         "N01" => "linear-advection",
         "N02" => "nonlinear-advection",
         "N03" => "diffusion",
